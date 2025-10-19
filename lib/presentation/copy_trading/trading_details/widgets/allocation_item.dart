e9c86dad95f9c3bf73124f75c01fea0e6c45ab46
wanogho joy <wanoghojoy@wanoghos-MacBook-Air.local>
2025-10-19 22:16:53 +0100

    pie_chart_animation

--- lib/presentation/copy_trading/trading_details/widgets/allocation_item.dart
+++ lib/presentation/copy_trading/trading_details/widgets/allocation_item.dart
@@ -1,12 +1,12 @@
 // ignore_for_file: deprecated_member_use
 
-import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:trading_app/helper_files/constants.dart';
+import 'package:trading_app/helper_files/extensions.dart';
 import 'package:trading_app/shared_widgets/filter_widget.dart';
 import 'package:trading_app/theme/app_textstyle.dart';
-
+import 'package:pie_chart/pie_chart.dart';
 import 'package:trading_app/theme/colors.dart';
 
 class AllocationItem extends StatelessWidget {
@@ -33,56 +33,96 @@ class AllocationItem extends StatelessWidget {
   }
 }
 
-class AppPieChart extends StatelessWidget {
+class AppPieChart extends StatefulWidget {
   const AppPieChart({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Stack(
-      alignment: Alignment.center,
-      children: [
-        PieChart(
-          swapAnimationDuration: Duration(milliseconds: 500),
-          PieChartData(
-            startDegreeOffset: -90,
-            centerSpaceRadius: 65,
-            centerSpaceColor: AppColors.scaffoldBgColor,
-            sectionsSpace: 0,
-            sections: [
-              PieChartSectionData(radius: 30, color: Color(0xffFBC98D)),
-            ],
-          ),
-        ),
+  State<AppPieChart> createState() => _AppPieChartState();
+}
 
-        PieChart(
-          swapAnimationDuration: Duration(milliseconds: 500),
-          PieChartData(
-            centerSpaceRadius: 70,
-            centerSpaceColor: AppColors.scaffoldBgColor,
-            sectionsSpace: 0,
-            sections: [
-              PieChartSectionData(
-                radius: 6,
-                color: AppColors.pieChartInnerYellow,
-              ),
-            ],
+class _AppPieChartState extends State<AppPieChart> {
+  Map<String, double> dataMap = {"Pro_trader": 5};
+  final colorList = [Color(0xffFBC98D)];
+  double _centerOpacity = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(Duration(milliseconds: 1000), () {
+      if (mounted) {
+        setState(() {
+          _centerOpacity = 1.0;
+        });
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PieChart(
+      dataMap: dataMap,
+      animationDuration: Duration(milliseconds: 2000),
+
+      chartRadius: context.deviceHeight / 2.2,
+      colorList: colorList,
+      initialAngleInDegree: 0,
+      chartType: ChartType.ring,
+      ringStrokeWidth: 32,
+
+      centerWidget: AnimatedOpacity(
+        opacity: _centerOpacity,
+        duration: Duration(milliseconds: 500),
+        child: Container(
+          decoration: BoxDecoration(
+            color: AppColors.pieChartInnerYellow,
+            shape: BoxShape.circle,
+          ),
+          width: 200,
+          height: 200,
+
+          child: Container(
+            margin: EdgeInsets.all(5),
+            decoration: BoxDecoration(
+              color: AppColors.scaffoldBgColor,
+              shape: BoxShape.circle,
+            ),
+            width: 150,
+            height: 150,
+            alignment: Alignment.center,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "BTCUSDT",
+                  style: const TextStyle(
+                    color: AppColors.primaryColor,
+                    fontSize: 12,
+                  ),
+                ),
+                const SizedBox(height: 8),
+                Text("100.00%", style: header2),
+              ],
+            ),
           ),
         ),
-        Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(
-              "BTCUSDT",
-              style: const TextStyle(
-                color: AppColors.primaryColor,
-                fontSize: 12,
-              ),
-            ),
-            const SizedBox(height: 8),
-            Text("100.00%", style: header2),
-          ],
-        ),
-      ],
+      ),
+
+      legendOptions: LegendOptions(
+        showLegendsInRow: false,
+        legendPosition: LegendPosition.right,
+        showLegends: false,
+        legendShape: BoxShape.circle,
+        legendTextStyle: TextStyle(fontWeight: FontWeight.bold),
+      ),
+      chartValuesOptions: ChartValuesOptions(
+        showChartValueBackground: false,
+        showChartValues: false,
+        showChartValuesInPercentage: false,
+        showChartValuesOutside: false,
+        decimalPlaces: 1,
+      ),
     );
   }
 }
